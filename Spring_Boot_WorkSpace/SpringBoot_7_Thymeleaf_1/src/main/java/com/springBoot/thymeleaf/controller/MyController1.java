ab2a2817a881c79b5713ec6af797660517487562
RITIK SHARMA <ritiksharma555598@gmail.com>
2025-01-19 01:09:52 +0530

    adding thymeleave tutorial

--- Spring_Boot_WorkSpace/SpringBoot_7_Thymeleaf_1/src/main/java/com/springBoot/thymeleaf/controller/MyController1.java
+++ Spring_Boot_WorkSpace/SpringBoot_7_Thymeleaf_1/src/main/java/com/springBoot/thymeleaf/controller/MyController1.java
@@ -1,7 +1,10 @@
 package com.springBoot.thymeleaf.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -19,6 +22,38 @@ public class MyController1 {
 		
 		return "about";
 		
+	}
+	
+	@GetMapping("example-iterate")
+	public String iterateHandler(Model model) {
+		
+		List<String> listOfData = List.of("Ankit sharma", "Ritik sharma", "Mahima Sharma", "Priyanshu sharma",
+				"Mamta devi", "Shyam Babu Sharma");
+		
+		model.addAttribute( "allNames",listOfData);
+		
+		return "iterate";
+	}
+	
+	
+	// handler for the conditional statements in thymeleaf
+	@GetMapping("example-conditinal")
+	public String ConditionalStatement(Model model) {
+		System.out.println("this is the ConditionalStatement handlar ");
+		
+		// this is for Elvis type of conditional statements
+		model.addAttribute("isActive", false);
+		
+		// this is for if-Unless type of conditional statements
+		model.addAttribute("gender", "male");
+		
+		// this is for switch 
+		
+		List<Integer> listOfInteger = List.of(123,342,456,333,564,222);
+		
+		model.addAttribute("mylist",listOfInteger);
+		
+		return "conditinal_3";
 	}
 
 }
